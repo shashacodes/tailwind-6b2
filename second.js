be4f1177b96206cfd562421e7ab0f7482175eba7
Ibanga Sharon <ibsharon19@gmail.com>
2023-04-25 13:07:45 -0700

    feat: add new changes

---
+++ second.js
@@ -0,0 +1,13 @@
+//FUNCTIONS
+const text = document.querySelector('#truncatedText')
+
+
+function truncatedString(value, length){
+    if(value.length > length) {
+     return `${value.slice(0, length)} <span style="color: blue; text-decoration: underline;">Read More</span>`
+    }
+
+    return value
+}
+
+text.innerHTML = truncatedString(text.textContent, 206)
